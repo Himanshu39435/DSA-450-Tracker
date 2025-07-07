ad1dcdf3ecff9e4d0040d31e98ac268c32de7a99
Himanshu39435 <himanshu39435@gmail.com>
2025-07-07 23:21:43 +0530

    Updates

---
+++ precache-manifest.33084fb38598654494519328b842bc41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8033d858e6bb82d38f43b3a757aa5e",
+    "url": "/DSA-450-Tracker/index.html"
+  },
+  {
+    "revision": "237a34c0a3290a13ee01",
+    "url": "/DSA-450-Tracker/static/css/2.7e784593.chunk.css"
+  },
+  {
+    "revision": "73f7377a3f48c1298b9a",
+    "url": "/DSA-450-Tracker/static/css/main.18022aad.chunk.css"
+  },
+  {
+    "revision": "237a34c0a3290a13ee01",
+    "url": "/DSA-450-Tracker/static/js/2.cba589b0.chunk.js"
+  },
+  {
+    "revision": "f89add3a9e987ffa91fbca42eb051d46",
+    "url": "/DSA-450-Tracker/static/js/2.cba589b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f7377a3f48c1298b9a",
+    "url": "/DSA-450-Tracker/static/js/main.0d61d4ae.chunk.js"
+  },
+  {
+    "revision": "8a9179df110911b76422",
+    "url": "/DSA-450-Tracker/static/js/runtime-main.b535b49e.js"
+  }
+]);
