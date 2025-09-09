0c4ee95c06f038615d2beab13bbaa21ffbd0eed6
Himanshu Singh <hk39435@gmail.com>
2025-09-10 03:43:18 +0530

    Upload production build folder

---
+++ build/precache-manifest.88f39ea6f7b94d8bdd96940174274c46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b244596f9523b59551a0eb59ce7183f",
+    "url": "/DSA-450-Tracker/index.html"
+  },
+  {
+    "revision": "dec1591f89b78e4dc7be",
+    "url": "/DSA-450-Tracker/static/css/2.7e784593.chunk.css"
+  },
+  {
+    "revision": "349e0d471a4a000e7b7c",
+    "url": "/DSA-450-Tracker/static/css/main.18022aad.chunk.css"
+  },
+  {
+    "revision": "dec1591f89b78e4dc7be",
+    "url": "/DSA-450-Tracker/static/js/2.e236d28b.chunk.js"
+  },
+  {
+    "revision": "f89add3a9e987ffa91fbca42eb051d46",
+    "url": "/DSA-450-Tracker/static/js/2.e236d28b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349e0d471a4a000e7b7c",
+    "url": "/DSA-450-Tracker/static/js/main.71ea5b77.chunk.js"
+  },
+  {
+    "revision": "8a9179df110911b76422",
+    "url": "/DSA-450-Tracker/static/js/runtime-main.b535b49e.js"
+  }
+]);
